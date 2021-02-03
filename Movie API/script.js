e1f146bb9f96a363a11eb3a7cb9808553e4a9c4b
Clemens Bruesch <clemensbruesch@gmail.com>
2021-02-04 00:27:15 +0100

    basic URLs

---
+++ Movie API/script.js
@@ -0,0 +1,5 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0018f2ed5cf91a94365b4ff9629631be&page=1';
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=0018f2ed5cf91a94365b4ff9629631be&query="';
