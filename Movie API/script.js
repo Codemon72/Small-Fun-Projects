7f8cf1a3f3e520118e5493644cf3ae5ce9889906
Clemens Bruesch <clemensbruesch@gmail.com>
2021-02-06 08:33:56 +0100

    curl ok

--- Movie API/script.js
+++ Movie API/script.js
@@ -3,3 +3,18 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 
 const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=0018f2ed5cf91a94365b4ff9629631be&query="';
+
+
+
+
+
+// Get initial movies
+getMovies(API_URL)
+
+async function getMovies(url) {
+  const res = await fetch(url)
+  const data = await res.json()
+
+  console.log(data.results)
+}
+
