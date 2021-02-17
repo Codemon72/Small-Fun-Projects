74a0d87c7f4cc97808cb754c369cfe4d9b2e06e0
Clemens Bruesch <clemensbruesch@gmail.com>
2021-02-17 09:20:19 +0100

    clock fully functioning

---
+++ JS Clock/script.js
@@ -0,0 +1,22 @@
+const pace = () => setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+const setClock = () => {
+  const currentDate = new Date();
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = currentDate.getHours() / 12
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+const setRotation = (element, rotationRatio) => {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+pace()
+setClock()
