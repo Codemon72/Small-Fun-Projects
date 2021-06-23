92b29c83b5cf7508be5c605f0b23775bd77e422a
Clemens Bruesch <clemensbruesch@gmail.com>
2021-06-23 20:35:32 +0200

    ToDo App working with local storage

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,37 +1,65 @@
-const form = document.getElementById('form');
-const input = document.getElementById('input');
-const todosUL = document.getElementById('todos');
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const todosUL = document.getElementById('todos')
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  addTodo();
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+if(todos) {
+    todos.forEach(todo => addTodo(todo))
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addTodo()
 })
 
-const addTodo = (todo) => {
-  let todoText = input.value;
+function addTodo(todo) {
+    let todoText = input.value
 
-  if(todo) {
-    todoText = todo.text
-  }
-
-  if(todoText) {
-    const todoEl = document.createElement('li');
-    if (todo && todo.completed){
-      todoEl.classList.add('completed')
+    if(todo) {
+        todoText = todo.text
     }
-    todoEl.innerText = todoText;
 
-    todoEl.addEventListener('click', () => {
-      todoEl.classList.toggle('completed')
-    });
+    if(todoText) {
+        const todoEl = document.createElement('li')
+        if(todo && todo.completed) {
+            todoEl.classList.add('completed')
+        }
 
-    todoEl.addEventListener('contextmenu', (e) => {
-      e.preventDefault();
-      todoEl.remove();
-    });
+        todoEl.innerText = todoText
 
-    todosUL.appendChild(todoEl);
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed')
+            updateLS()
+        }) 
 
-    input.value = ''; 
-  }
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault()
+
+            todoEl.remove()
+            updateLS()
+        }) 
+
+        todosUL.appendChild(todoEl)
+
+        input.value = ''
+
+        updateLS()
+    }
+}
+
+function updateLS() {
+    todosEl = document.querySelectorAll('li')
+
+    const todos = []
+
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        })
+    })
+
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
