15ef038ad12787d281a9e74eeec3f0541bcadebe
Clemens Bruesch <clemensbruesch@gmail.com>
2021-06-26 19:13:02 +0200

    click adds list item to list

--- ToDoList2/script.js
+++ ToDoList2/script.js
@@ -1,1 +1,33 @@
-console.log('blib');
+// SELECTORS
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// EVENT LISTENERS
+todoButton.addEventListener('click', addTodo);
+
+// FUNCTIONS
+function addTodo (event) {
+  event.preventDefault();
+  // create div
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+  // create li
+  const newTodo = document.createElement('li');
+  newTodo.innerText = 'hey';
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+  // check 'completed' button
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add('complete-btn');
+  todoDiv.appendChild(completedButton);
+  // check 'trash' button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add('trash-btn');
+  todoDiv.appendChild(trashButton);
+  // append to list
+  todoList.appendChild(todoDiv);
+
+}
