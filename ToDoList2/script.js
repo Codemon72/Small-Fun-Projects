5d038fdf57288680cd51837f37ad582942877ba6
Clemens Bruesch <clemensbruesch@gmail.com>
2021-06-26 19:24:07 +0200

    list items styled; input field cleared after appending

--- ToDoList2/script.js
+++ ToDoList2/script.js
@@ -1,33 +1,34 @@
 // SELECTORS
-const todoInput = document.querySelector('.todo-input');
-const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-list');
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
 // EVENT LISTENERS
-todoButton.addEventListener('click', addTodo);
+todoButton.addEventListener("click", addTodo);
 
 // FUNCTIONS
-function addTodo (event) {
+function addTodo(event) {
   event.preventDefault();
   // create div
-  const todoDiv = document.createElement('div');
-  todoDiv.classList.add('todo');
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
   // create li
-  const newTodo = document.createElement('li');
-  newTodo.innerText = 'hey';
-  newTodo.classList.add('todo-item');
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
   // check 'completed' button
-  const completedButton = document.createElement('button');
+  const completedButton = document.createElement("button");
   completedButton.innerHTML = '<i class="fas fa-check"></i>';
-  completedButton.classList.add('complete-btn');
+  completedButton.classList.add("complete-btn");
   todoDiv.appendChild(completedButton);
   // check 'trash' button
-  const trashButton = document.createElement('button');
+  const trashButton = document.createElement("button");
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add('trash-btn');
+  trashButton.classList.add("trash-btn");
   todoDiv.appendChild(trashButton);
   // append to list
   todoList.appendChild(todoDiv);
-
+  // clear input field
+  todoInput.value = "";
 }
