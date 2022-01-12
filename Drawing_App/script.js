075c4461baec10deaf441164f6da4f371133909d
Clemens Bruesch <clemensbruesch@gmail.com>
2022-01-12 21:45:52 +0100

    drawing with circles

--- Drawing_App/script.js
+++ Drawing_App/script.js
@@ -15,3 +15,24 @@ canvas.addEventListener("mousedown", (e) => {
 
   console.log(x, y)
 })
+
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+  x = undefined;
+  y = undefined;
+})
+
+canvas.addEventListener("mousemove", (e)=> {
+  draw(e.offsetX, e.offsetY)
+})
+
+c.fillStyle = "black"
+
+function draw(x2, y2){
+  if(isDrawing){
+    c.beginPath();
+    c.arc(x2,y2, penSize,0 ,Math.PI * 2);
+    c.closePath();
+    c.fill()
+  }
+}
