111c10553c647c4247c13768197a2a6914dfb61d
Clemens Bruesch <clemensbruesch@gmail.com>
2024-02-11 00:09:12 +0100

    chore: apply linting rules to enforce single quotes and remove semicolons

--- Drawing_App/script.js
+++ Drawing_App/script.js
@@ -1,56 +1,56 @@
-const colorCircle = document.querySelectorAll(".color-circle");
+const colorCircle = document.querySelectorAll('.color-circle')
 
-let penSize = 10;
-let isDrawing;
-let x;
-let y;
+let penSize = 10
+let isDrawing
+let x
+let y
 
-var canvas = document.querySelector("canvas");
-c = canvas.getContext("2d");
+var canvas = document.querySelector('canvas')
+c = canvas.getContext('2d')
 
-canvas.addEventListener("mousedown", (e) => {
-  isDrawing = true;
-  x = e.offsetX;
-  y = e.offsetY;
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true
+  x = e.offsetX
+  y = e.offsetY
 
   console.log(x, y)
 })
 
-canvas.addEventListener("mouseup", () => {
-  isDrawing = false;
-  x = undefined;
-  y = undefined;
+canvas.addEventListener('mouseup', () => {
+  isDrawing = false
+  x = undefined
+  y = undefined
 })
 
-canvas.addEventListener("mousemove", (e)=> {
+canvas.addEventListener('mousemove', (e) => {
   draw(e.offsetX, e.offsetY)
 })
 
-c.fillStyle = "black";
-c.strokeStyle = c.fillStyle;
+c.fillStyle = 'black'
+c.strokeStyle = c.fillStyle
 
-function draw(x2, y2){
-  if(isDrawing){
-    c.beginPath();
-    c.arc(x2,y2, penSize,0 ,Math.PI * 2);
-    c.closePath();
+function draw(x2, y2) {
+  if (isDrawing) {
+    c.beginPath()
+    c.arc(x2, y2, penSize, 0, Math.PI * 2)
+    c.closePath()
     c.fill()
     // draw line
     drawLine(x, y, x2, y2)
   }
-  x = x2;
-  y = y2;
+  x = x2
+  y = y2
 }
 
 function drawLine(x1, y1, x2, y2) {
-  c.beginPath();
-  c.moveTo(x1, y1);
-  c.lineTo(x2, y2);
-  c.strokeStyle = "black";
-  c.lineWidth = penSize * 2;
-  c.stroke();
+  c.beginPath()
+  c.moveTo(x1, y1)
+  c.lineTo(x2, y2)
+  c.strokeStyle = c.fillStyle
+  c.lineWidth = penSize * 2
+  c.stroke()
 }
 
-document.querySelector(".fa-refresh"). addEventListener('click', function () {
-  c.clearRect(0,0, canvas.width, canvas.height)
+document.querySelector('.fa-refresh').addEventListener('click', function () {
+  c.clearRect(0, 0, canvas.width, canvas.height)
 })
